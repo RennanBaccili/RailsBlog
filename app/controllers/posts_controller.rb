67227b6ed4d95a1e4541ac8ce8d5c6733d08e54d
Rennan Bacili <rennanbaccili@gmail.com>
2023-12-06 08:51:43 -0300

    criando um crud manual, product

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -68,3 +68,4 @@ class PostsController < ApplicationController
       params.require(:post).permit(:title, :description)
     end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_06_094621) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_06_111449) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
